6200e6de42f4b4eb5eca5dfe74586d65b62466f2
Aniket Gauda <aniketgauda99@gmail.com>
2026-01-13 11:38:38 +0530

    Read Me File Changes

--- User Dashboard/UD_FrontEnd/scripts/manageUsers.js
+++ User Dashboard/UD_FrontEnd/scripts/manageUsers.js
@@ -1,11 +1,13 @@
 const modal = document.getElementById('modal');
-const saveBtn = document.getElementById('saveBtn');
+// const saveBtn = document.getElementById('saveBtn');
 
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 const roleInput = document.getElementById('role-input');
+
+const form = document.querySelector('form');
 
 function openModal() {
     modal.style.display = `flex`;
@@ -20,3 +22,28 @@ function closeModal() {
 
     roleInput.value = `user`;
 }
+
+form.addEventListener('submit',async function(e) {
+    e.preventDefault();
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const role = roleInput.value;
+
+    const response = await fetch('http://localhost:8000/createUser',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name, email, password, role})
+    })
+    const data = await response.json();
+
+    console.log('DATA FROM API CALL:: ',data);
+
+    if(data.status == 'false') {
+        return alert( `❌ ${data.err} ❌`)
+    }
+    alert(`${data.message} 🎉🎉🎉`);
+    closeModal();
+})
